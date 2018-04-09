a63a172fb074d8faa0ba15c304d53fa6d02c3f39
小明 <secretan@163.com>
2018-04-10 00:07:00 +0800

    增加测试程序
    mqtt变化

--- onenet/mqtt.py
+++ onenet/mqtt.py
@@ -1,28 +1,27 @@
 
-
-#Ԥ This is OneNet MQTT class
+# Ԥ This is OneNet MQTT class
 
 from umqtt_simple import MQTTClient
 from machine import Timer
 import json
 
-ip='183.230.40.39'
-port=6002
+ip ='183.230.40.39'
+port =6002
 
 class OneNetMQTT:
     global ip
     global port
-    def __init__(self, dev_id,product_id, api_key,sub_cba):
-        self.client=MQTTClient(dev_id,ip,port,product_id,api_key,0)
+    def __init__(self, dev_id ,product_id, api_key ,sub_cba):
+        self.client =MQTTClient(dev_id ,ip ,port ,product_id ,api_key ,0)
         self.client.set_callback(sub_cba)
         if (self.client.connect() == 1):
             print("Connect Server Success!")
-            self.connect_flag=True
+            self.connect_flag =True
             self.client.subscribe(b"home_info_mqtt")
         else:
             print("Connect Server Fail!")
         
-    def __deinit(self):
+    def __deinit__(self):
         if (self.connect_flag):
             self.client.disconnect()
             self.tim.deinit()
@@ -30,9 +29,9 @@ class OneNetMQTT:
         else:
             print("Nothing Done!")
             
-    def start_heartbeat(self,timeout):
-        self.tim=Timer(-1)
-        self.tim.init(period=timeout,mode=Timer.PERIODIC,callback=lambda t:print(1))
+    def start_heartbeat(self ,timeout):
+        self.tim =Timer(-1)
+        self.tim.init(period=timeout ,mode=Timer.PERIODIC ,callback=lambda t :print(1))
         
     
 # Received messages from subscriptions will be delivered to this callback
@@ -41,14 +40,14 @@ class OneNetMQTT:
         
         
     def publish3(self, msg):
-        topic_name='$dp'
-        stra=json.dumps(msg)
-        #stra=str(msg)
-        #print(str)
-        newb=b'\x03\x00\x00'
+        topic_nam e ='$dp'
+        stra =json.dumps(msg)
+        # stra=str(msg)
+        # print(str)
+        newb =b'\x03\x00\x00'
         newba = bytearray(newb)
-        newba[1] = (len(stra))>>8
+        newba[1] = (len(stra) )> >8
         newba[2] = len(stra)
-        newb=bytes(newba)+bytes(bytearray(stra))
+        newb =bytes(newba ) +bytes(bytearray(stra))
         print(newb)
-        self.client.publish(topic_name,newb)
+        self.client.publish(topic_name ,newb)
