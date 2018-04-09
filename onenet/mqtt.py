6e5b94520438de8369d9c4bcb57990a20e48d5db
小明 <secretan@163.com>
2018-04-10 00:08:39 +0800

    实现反馈函数能够打印东西
    实现使用i2c读取温度值上传数据

--- onenet/mqtt.py
+++ onenet/mqtt.py
@@ -1,27 +1,27 @@
 
-# Ԥ This is OneNet MQTT class
+#Ԥ This is OneNet MQTT class
 
 from umqtt_simple import MQTTClient
 from machine import Timer
-import json
+import json,machine
 
-ip ='183.230.40.39'
-port =6002
+ip='183.230.40.39'
+port=6002
 
 class OneNetMQTT:
     global ip
     global port
-    def __init__(self, dev_id ,product_id, api_key ,sub_cba):
-        self.client =MQTTClient(dev_id ,ip ,port ,product_id ,api_key ,0)
+    def __init__(self, dev_id,product_id, api_key,sub_cba):
+        self.client=MQTTClient(dev_id,ip,port,product_id,api_key,0)
         self.client.set_callback(sub_cba)
         if (self.client.connect() == 1):
             print("Connect Server Success!")
-            self.connect_flag =True
+            self.connect_flag=True
             self.client.subscribe(b"home_info_mqtt")
         else:
             print("Connect Server Fail!")
         
-    def __deinit__(self):
+    def __deinit(self):
         if (self.connect_flag):
             self.client.disconnect()
             self.tim.deinit()
@@ -29,25 +29,32 @@ class OneNetMQTT:
         else:
             print("Nothing Done!")
             
-    def start_heartbeat(self ,timeout):
-        self.tim =Timer(-1)
-        self.tim.init(period=timeout ,mode=Timer.PERIODIC ,callback=lambda t :print(1))
-        
     
 # Received messages from subscriptions will be delivered to this callback
-    def sub_cb(topic, msg):
-        print((topic, msg))
+    def timeouta(self,t):
+        #print(t)
+        state = machine.disable_irq()
+        self.client.check_msg()
+        machine.enable_irq(state)
+        self.tim.init(period=self.timeout,mode=Timer.ONE_SHOT,callback=self.timeouta)
+        
+    def start_heartbeat(self,timeout):
+        self.timeout=timeout
+        self.tim=Timer(-1)
+        self.tim.init(period=timeout,mode=Timer.ONE_SHOT,callback=self.timeouta)
+        
         
         
     def publish3(self, msg):
-        topic_nam e ='$dp'
-        stra =json.dumps(msg)
-        # stra=str(msg)
-        # print(str)
-        newb =b'\x03\x00\x00'
+        topic_name='$dp'
+        stra=json.dumps(msg)
+        #stra=str(msg)
+        #print(str)
+        newb=b'\x03\x00\x00'
         newba = bytearray(newb)
-        newba[1] = (len(stra) )> >8
+        newba[1] = (len(stra))>>8
         newba[2] = len(stra)
-        newb =bytes(newba ) +bytes(bytearray(stra))
-        print(newb)
-        self.client.publish(topic_name ,newb)
+        newb=bytes(newba)+bytes(bytearray(stra))
+        #print(newb)
+        self.client.publish(topic_name,newb)
+        
